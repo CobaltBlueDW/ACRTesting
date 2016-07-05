bbf04ca4faf39e1b0c86c830244535dd113eec36
David Wipperfurth <cobaltbluedw@gmail.com>
2016-07-05 07:07:19 -0500

    Update test1.php

--- test1.php
+++ test1.php
@@ -1,3 +1,8 @@
 <?php
 
 print "this is a test.";
+
+//test commented code
+//print "this is a test of commented code";
+//echo "here, we continue the commented code test";
+//$mtrace = $and.$one.$more.$line.$of.$commented.$code;
